fc1f456ec04cf2ae5b0270c8154385f69e360edb
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-08 22:44:42 +0530

    Working onYourApplied jobs

--- src/pages/YourAppliedJobs.js
+++ src/pages/YourAppliedJobs.js
@@ -1,5 +1,52 @@
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+
 const YourAppliedJobs = () => {
-  return <>Your Applied Jobs</>;
+  const { user, url, jobs, fetchJobs } = useContext(AuthContext);
+
+  // Define a state variable to store the applied jobs data
+  const [appliedJobs, setAppliedJobs] = useState([]);
+
+  // Fetch applied jobs data when the component mounts
+  useEffect(() => {
+    const fetchAppliedJobs = async () => {
+      try {
+        const response = await fetch(`${url}/jobs/appliedJobs`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+
+          console.log(data);
+          setAppliedJobs(data);
+        } else {
+          // Handle errors if needed
+        }
+      } catch (error) {
+        console.error("Error fetching applied jobs:", error);
+      }
+    };
+
+    fetchAppliedJobs(); // Call the fetch function when the component mounts
+  }, []);
+
+  return (
+    <>
+      <h2>Your Applied Jobs</h2>
+      <ul>
+        {appliedJobs.map((job) => (
+          <li key={job._id}>
+            <h4>{job.title}</h4>
+            <p>{job.description}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default YourAppliedJobs;
