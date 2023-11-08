3ea3b0c1687684df8cd0c15184307581c265ba10
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-08 19:13:26 +0530

    made changes

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,4 +1,34 @@
+import React, { useEffect, useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+//
 const Job = ({ job }) => {
+  const { user, url } = useContext(AuthContext);
+  //
+  const applyHandler = async () => {
+    try {
+      const response = await fetch(`${url}/jobs/apply/${job._id}`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        // Successful application
+        alert("Job application successful");
+      } else if (response.status === 403) {
+        alert("Permission denied");
+      } else if (response.status === 400) {
+        alert("Job already applied");
+      } else {
+        alert("Error applying for the job");
+      }
+    } catch (error) {
+      alert("Error applying for the job");
+    }
+  };
+  //
   return (
     <div key={job._id}>
       <h4>{job.title}</h4>
@@ -6,6 +36,8 @@ const Job = ({ job }) => {
       <p>Description:{job.description}</p>
       <p>Posted on : {job.createdAt}</p>
       <p>Total applicant:{job.appliedBy.length}</p>
+      {/*  */}
+      <button onClick={applyHandler}>Apply</button>
     </div>
   );
 };
