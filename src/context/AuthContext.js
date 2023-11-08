9284b42af74d9d86161cbab8fa8bbdc7eca4a6a6
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-08 15:23:31 +0530

    AuthContext

---
+++ src/context/AuthContext.js
@@ -0,0 +1,42 @@
+import { createContext, useState, useEffect } from "react";
+const AuthContext = createContext();
+//
+const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  //
+  const login = (userData) => {
+    localStorage.setItem("user", JSON.stringify(userData));
+    //
+    setUser(userData);
+  };
+  //
+  useEffect(() => {
+    //
+    const fetchUserLS = async () => {
+      try {
+        // console.log("calling fetchUserLS ");
+        const user = localStorage.getItem("user");
+        //
+        setUser(user);
+      } catch (err) {
+        console.error("Error fetching data in AuthContext:", err);
+      }
+      //
+    };
+
+    // console.log("user: in fetchUserLS ", user);
+    // calling the function-
+    fetchUserLS();
+    //
+  }, [user]);
+
+  return (
+    <>
+      <AuthContext.Provider value={{ user, login }}>
+        {children}
+      </AuthContext.Provider>
+    </>
+  );
+};
+
+export { AuthContext, AuthContextProvider };
