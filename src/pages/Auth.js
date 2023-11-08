f0411f1cbdcab36c79435dab49993da90fd4dbc7
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-09 00:12:34 +0530

    auth.js

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -38,6 +38,46 @@ const Auth = () => {
     }
   };
   //
+  const registrationHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("registrationEmail").value;
+    const password = document.getElementById("registrationPassword").value;
+    //
+    const cvUrl = document.getElementById("registrationCV").value;
+    //
+    const userType = document.getElementById("userType").value; // Get the selected user type
+
+    try {
+      const response = await fetch(`${url}/users`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          cvUrl: cvUrl,
+          userType: userType, // Include the selected user type
+        }),
+      });
+
+      console.log(response, "response");
+
+      if (response.ok) {
+        const registrationResult = await response.json();
+
+        if (registrationResult.token) {
+          login(registrationResult);
+        }
+      } else {
+        window.alert("Registration failed. Please check your input.");
+      }
+    } catch (err) {
+      console.log(err);
+      window.alert("An error occurred during registration. Please try again.");
+    }
+  };
+
+  //
   return (
     <div>
       <h1>Auth</h1>
@@ -49,6 +89,23 @@ const Auth = () => {
         <label>Password</label>
         <input id="loginPassword" type="text" required />
         <button type="submit">Login</button>
+      </form>
+      {/*  */}
+      <h2>Registration form</h2>
+      <form onSubmit={registrationHandler}>
+        <label>Email</label>
+        <input id="registrationEmail" type="text" required />
+        <label>Password</label>
+        <input id="registrationPassword" type="text" required />
+        <label>CV url</label>
+        <input id="registrationCV" type="text" required />
+        <label>Select User Type</label>
+        <select id="userType">
+          <option value="applicant">Applicant</option>
+          <option value="jobcreator">Job Creator</option>
+        </select>
+
+        <button type="submit">Register</button>
       </form>
     </div>
   );
