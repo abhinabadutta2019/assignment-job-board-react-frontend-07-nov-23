b11bf40bce535f9303f3f5e48a122c582a7b3c4a
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-09 13:33:18 +0530

    dd

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,13 @@
+import React from "react";
+import { Container, Alert } from "react-bootstrap";
+
 const NotFound = () => {
   return (
-    <div>
-      <h2>Not found, try other page</h2>
-    </div>
+    <Container className="mt-4">
+      <Alert variant="danger">
+        <h2>Not found, please try another page.</h2>
+      </Alert>
+    </Container>
   );
 };
 
