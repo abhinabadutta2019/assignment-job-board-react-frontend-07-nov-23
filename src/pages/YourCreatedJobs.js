c3ede79d273003eace723659f696249e4bdb06f2
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-09 13:03:22 +0530

    bootstrap added to Your createdJobs

--- src/pages/YourCreatedJobs.js
+++ src/pages/YourCreatedJobs.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useContext } from "react";
 import Job from "../components/Job";
 import { AuthContext } from "../context/AuthContext";
+import { Container } from "react-bootstrap";
+//
 const YourCreatedJobs = () => {
   //
   const { yourCreatedJobs, fetchYourCreatedJobs } = useContext(AuthContext);
@@ -10,16 +12,18 @@ const YourCreatedJobs = () => {
   }, []);
   //
   return (
-    <>
-      Your Created Jobs
-      <ul>
-        {yourCreatedJobs.length > 0 ? (
-          yourCreatedJobs.map((job) => <Job key={job._id} job={job} />)
-        ) : (
-          <p>No jobs available</p>
-        )}
-      </ul>
-    </>
+    <Container>
+      <h2 className="mt-4">Your Created Jobs</h2>
+      {yourCreatedJobs.length > 0 ? (
+        yourCreatedJobs.map((job) => (
+          <div key={job._id} className="mb-3">
+            <Job job={job} />
+          </div>
+        ))
+      ) : (
+        <p>No jobs available</p>
+      )}
+    </Container>
   );
 };
 
