8a0e1f65b23b01c1e8c73c289b478dc153280384
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-11-09 13:13:41 +0530

    CreateJob.js bootstrap

--- src/pages/CreateJob.js
+++ src/pages/CreateJob.js
@@ -1,20 +1,19 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { Button, Form, Container, Row, Col } from "react-bootstrap";
 
 const CreateJob = () => {
-  //
   const { user, url } = useContext(AuthContext);
-  //
+
   const formHandler = async (event) => {
     event.preventDefault();
-    //
+
     const titleElement = document.getElementById("title");
     const descriptionElement = document.getElementById("description");
-    //
+
     const title = titleElement.value;
     const description = descriptionElement.value;
 
-    //
     try {
       const response = await fetch(`${url}/jobs`, {
         method: "POST",
@@ -29,9 +28,7 @@ const CreateJob = () => {
       });
 
       if (response.ok) {
-        // Form submission successful
-        window.alert("Task created successfully"); // Show a success alert
-        // Clear the textarea
+        window.alert("Job created successfully");
         titleElement.value = "";
         descriptionElement.value = "";
       } else if (response.status === 403) {
@@ -39,27 +36,31 @@ const CreateJob = () => {
       }
     } catch (error) {
       console.error(error);
-      alert("Error applying for the job");
-      //   if (error && error.code == 11000) {
-      //     window.alert("Duplicate job title- use another title");
-      //   } else {
-      //     window.alert("An error occurred. Please try again.");
-      //   }
+      alert("Error creating the job");
     }
   };
-  //
+
   return (
-    <div>
-      <h2>Create Job post</h2>
-      <form onSubmit={formHandler}>
-        {/* <form> */}
-        <label>Title</label>
-        <input id="title" type="text" required />
-        <label>Description</label>
-        <textarea id="description" type="text" required />
-        <button type="submit">Submit here</button>
-      </form>
-    </div>
+    <Container>
+      <h2 className="mt-4">Create Job post</h2>
+      <Row>
+        <Col md={4}>
+          <Form onSubmit={formHandler}>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control type="text" required />
+            </Form.Group>
+            <Form.Group controlId="description">
+              <Form.Label>Description</Form.Label>
+              <Form.Control as="textarea" required />
+            </Form.Group>
+            <Button type="submit" variant="success">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
